f735c8171ecdf9041d497e2436862dda2867b995
AzulRK22 <97193812+AzulRK22@users.noreply.github.com>
2025-01-12 12:34:07 -0600

    mejoras diseño reservaciones
    falta añadir estado

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './BookingForm.css';
 
 function BookingForm() {
     const [formData, setFormData] = useState({
--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import './BookingPage.css';
 import BookingForm from './BookingForm';
-
 
 function BookingPage() {
   return (
